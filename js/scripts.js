a8b9b47565b4059834d9231803f13edda35e9338
Szymon Feliks <sz.feliks@gmail.com>
2017-02-07 14:13:23 +0100

    add js/

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+// plik scripts.js task_8.3
+// komentarze blokowe i liniowe
+
+var name = prompt('Wpisz swoje imię');
+
+// zakomentowanie funkcji alert komentarzem liniowym
+// alert('Witaj, ' + name);
+
+// zakomentowanie funkcji console.log komentarzem blokowym
+/*
+console.log('Witaj, ' + name);
+*/
